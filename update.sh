9a1952d19b916eb25f638c33144b67d9b7b9ba7f
YAKM0 <112671796+YAKM0@users.noreply.github.com>
2022-09-01 21:40:35 -0500

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+{ echo "Starting updates in background."; } 2> /dev/null
+sudo apt-get update -y > /dev/null 2>&1
+sudo apt-get upgrade -y > /dev/null 2>&1
+
+while true; do
+        read -p "Would you like to update Kali Distribution?" yn
+        case $yn in 
+                [Yy]* ) sudo apt dist-upgrade -y; break;;
+                [Nn]* ) exit;;
+                * ) echo "Please answer yes or no.";;
+        esac
+done
+sudo apt --fix-broken install -y
+sudo dpkg --configure -a
+sudo apt autoremove -y > /dev/null 2>&1
+{ echo "Updates complete."; } 2> /dev/null
