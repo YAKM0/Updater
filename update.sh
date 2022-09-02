778c0323c1b09b9465ff1c5f9aba5b45368b660d
YAKM0 <112671796+YAKM0@users.noreply.github.com>
2022-09-01 21:43:17 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,14 +4,14 @@ sudo apt-get update -y > /dev/null 2>&1
 sudo apt-get upgrade -y > /dev/null 2>&1
 
 while true; do
-        read -p "Would you like to update Kali Distribution?" yn
+        read -p "Would you like to update Kali Distribution? " yn
         case $yn in 
                 [Yy]* ) sudo apt dist-upgrade -y; break;;
                 [Nn]* ) exit;;
                 * ) echo "Please answer yes or no.";;
         esac
 done
-sudo apt --fix-broken install -y
-sudo dpkg --configure -a
+sudo apt --fix-broken install -y > /dev/null 2>&1
+sudo dpkg --configure -a > /dev/null 2>&1 
 sudo apt autoremove -y > /dev/null 2>&1
 { echo "Updates complete."; } 2> /dev/null
